3d71c0a1d5374ffc976cd2badc6de5a5f9a57663
Devon Sawatsky <novedevo@gmail.com>
2021-12-03 15:56:09 -0800

    another nonworking attempt

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,3 +1,5 @@
+use std::iter::repeat;
+
 fn main() {
     let input = include_str!("../../data/day3_test.txt").lines();
     let (gamma, epsilon) = generate_gamma_epsilon(input.clone());
@@ -8,34 +10,29 @@ fn main() {
     );
     println!("gamma ({}) * epsilon ({}) = {}", g, e, g * e);
 
-    let _o2 = recursive_filter(input, 0, '1');
+    let _o2 = recursive_filter(input.clone().collect(), 0, '1');
+    // let co2 = recursive_filter(input.collect(), 0, '0');
+    // println!("{} {}", _o2, co2)
 }
 
 fn generate_gamma_epsilon<'a>(input: impl Iterator<Item = &'a str> + Clone) -> (String, String) {
     let length = input.clone().count();
-    let mut i = 0;
-    let mut popcount = [0; 12];
-    'outer: loop {
-        for line in input.clone() {
-            if let Some(digit) = line.chars().nth(i) {
-                if digit == '1' {
-                    popcount[i] += 1
-                }
-            } else {
-                break 'outer;
-            }
-        }
-        i += 1;
-    }
+    let popcount: Vec<usize> =
+        input
+            .fold(
+                Box::new(repeat(0usize)) as Box<dyn std::iter::Iterator<Item = usize>>,
+                |acc, current| {
+                    Box::new(current.chars().zip(acc).map(|(current_char, acc)| {
+                        acc + current_char.to_digit(2).unwrap() as usize
+                    }))
+                },
+            )
+            .collect();
     let gamma = popcount
-        .get(..i)
-        .unwrap()
         .iter()
         .map(|count| if count * 2 >= length { '1' } else { '0' })
         .collect::<String>();
     let epsilon = popcount
-        .get(..i)
-        .unwrap()
         .iter()
         .map(|count| if count * 2 >= length { '0' } else { '1' })
         .collect::<String>();
@@ -43,14 +40,11 @@ fn generate_gamma_epsilon<'a>(input: impl Iterator<Item = &'a str> + Clone) -> (
     (gamma, epsilon)
 }
 
-fn recursive_filter<'a>(
-    mut input: impl Iterator<Item = &'a str> + Clone,
-    index: usize,
-    preferred_digit: char,
-) -> &'a str {
-    let (gamma, epsilon) = generate_gamma_epsilon(input.clone());
+fn recursive_filter(input: Vec<&str>, index: usize, preferred_digit: char) -> &str {
+    let input = input.into_iter();
+    let (gamma, epsilon) = dbg!(generate_gamma_epsilon(input.clone()));
     if index >= gamma.chars().count() {
-        return input.next().unwrap();
+        return input.clone().next().unwrap();
     }
     let input = input.filter(|item| {
         let (current, (gamma_char, epsilon_char)) = item
@@ -61,10 +55,10 @@ fn recursive_filter<'a>(
 
         let (most, least) = (current == gamma_char, current == epsilon_char);
         if most && least {
-            current == preferred_digit
+            dbg!(current == preferred_digit)
         } else {
             most
         }
     });
-    recursive_filter(input, index, preferred_digit)
+    recursive_filter(input.collect(), index + 1, preferred_digit)
 }
