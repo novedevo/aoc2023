c77df12f78afc5b63a5b5a720e189251a2001108
Devon Sawatsky <novedevo@gmail.com>
2021-12-22 17:16:47 -0800

    cleanup day6

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -4,18 +4,12 @@ fn main() {
     let input = include_str!("../../data/day6.txt")
         .split(',')
         .map(|fish| fish.parse::<u8>().unwrap());
-    // let mut fish_count = [0; 7];
-    // for fish in input {
-    //     fish_count[fish] += 1
-    // }
     let mut fish_counter = 0usize;
     let mut cache = HashMap::new();
-    let time_before = std::time::Instant::now();
     for fish in input {
         fish_counter += simulate_fish(fish, 80, &mut cache);
     }
-    let elapsed = time_before.elapsed();
-    println!("{} took {}", fish_counter, elapsed.as_nanos());
+    println!("{}", fish_counter);
 }
 
 fn simulate_fish(timer: u8, days_remaining: u16, cache: &mut HashMap<(u8, u16), usize>) -> usize {
