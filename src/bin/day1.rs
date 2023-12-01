497293678f1afade718011bdd8c24072acbb3df7
Devon Sawatsky <novedevo@gmail.com>
2023-12-01 15:58:45 -0800

    day1 part 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,41 +1,45 @@
-fn count_increases(input: &str) -> i32 {
+fn sum_digits(input: &str) -> i32 {
     input
         .lines()
-        .map(|s| s.parse().unwrap())
-        .fold((0, i32::MAX), |(sum, prev), depth| {
-            (if depth > prev { sum + 1 } else { sum }, depth)
+        .map(|s| {
+            s.chars()
+                .filter(char::is_ascii_digit)
+                .collect::<Vec<char>>()
         })
-        .0
+        .map(|v| {
+            [v.first().unwrap(), v.last().unwrap()]
+                .into_iter()
+                .collect::<String>()
+        })
+        .map(|s| s.parse::<i32>().unwrap())
+        .sum()
 }
 
-fn count_increases_sliding_window(input: &str) -> u32 {
-    input
-        .lines()
-        .map(|s| s.parse().unwrap())
-        .fold(
-            (0, (u16::MAX as u32, u16::MAX as u32, u16::MAX as u32)),
-            |(sum, (ante, pen, ult)), depth| {
-                (
-                    if depth + ult + pen > ult + pen + ante {
-                        sum + 1
-                    } else {
-                        sum
-                    },
-                    (pen, ult, depth),
-                )
-            },
-        )
-        .0
-}
+// fn count_increases_sliding_window(input: &str) -> u32 {
+//     input
+//         .lines()
+//         .map(|s| s.parse().unwrap())
+//         .fold(
+//             (0, (u16::MAX as u32, u16::MAX as u32, u16::MAX as u32)),
+//             |(sum, (ante, pen, ult)), depth| {
+//                 (
+//                     if depth + ult + pen > ult + pen + ante {
+//                         sum + 1
+//                     } else {
+//                         sum
+//                     },
+//                     (pen, ult, depth),
+//                 )
+//             },
+//         )
+//         .0
+// }
 
 fn main() {
     let input = include_str!("../../data/day1.txt");
-    println!(
-        "using simple comparison, depth increased {} times",
-        count_increases(input)
-    );
-    println!(
-        "using a sliding window, depth increased {} times",
-        count_increases_sliding_window(input)
-    );
+    println!("sum of first and second digits is {}", sum_digits(input));
+    // println!(
+    //     "using a sliding window, depth increased {} times",
+    //     count_increases_sliding_window(input)
+    // );
 }
