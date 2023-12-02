6255b1d48b546019b13da65b4b3efb55e26f7a80
Devon Sawatsky <novedevo@gmail.com>
2023-12-01 17:49:19 -0800

    add an incorrect regex-only solution

---
+++ src/bin/day1_invalidregex.rs
@@ -0,0 +1,86 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+use regex::Regex;
+
+fn sum_digits(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|s| {
+            s.chars()
+                .filter(char::is_ascii_digit)
+                .collect::<Vec<char>>()
+        })
+        .map(|v| {
+            [v.first().unwrap(), v.last().unwrap()]
+                .into_iter()
+                .collect::<String>()
+        })
+        .map(|s| s.parse::<i32>().unwrap())
+        .sum()
+}
+
+fn sum_numbers(input: &str) -> u32 {
+    let map: HashMap<&str, u32> = [
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]
+    .into();
+
+    let numbers: String = map.keys().fold(
+        String::with_capacity(map.keys().map(|k| k.len() + 1).sum()),
+        |mut acc, elem| {
+            if !acc.is_empty() {
+                acc.push('|')
+            }
+            acc.push_str(elem);
+            acc
+        },
+    );
+
+    // let first_pattern = format!("({})|(\\d)", numbers);
+    // let last_pattern = format!(".*(({})|(\\d))", numbers);
+    let pattern = format!("((?:{numbers})|(?:\\d))");
+
+    let regex = Regex::new(format!("{pattern}.*{pattern}?").as_str()).unwrap();
+    input
+        .lines()
+        .map(|s| {
+            regex
+                .captures(s)
+                .unwrap()
+                .iter()
+                .flatten()
+                .skip(1)
+                .map(|m| m.as_str())
+                .map(|s| str_to_digit(s, &map))
+                .collect_vec()
+        })
+        .map(|v| (*v.first().unwrap(), *v.last().unwrap_or(v.first().unwrap())))
+        .map(|(first, last)| 10 * first + last)
+        .sum()
+}
+
+fn str_to_digit(s: &str, map: &HashMap<&str, u32>) -> u32 {
+    if s.len() == 1 {
+        s.parse().unwrap()
+    } else {
+        *map.get(s).unwrap()
+    }
+}
+
+fn main() {
+    let input = include_str!("../../data/day1.txt");
+    // println!("sum of first and second digits is {}", sum_digits(input));
+    println!(
+        "tracking spelled out numbers, sum of first and second digits is {}",
+        sum_numbers(input)
+    );
+}
