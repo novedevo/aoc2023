66c680f8b488ef37d08a4f476e95f03498b052b1
Devon Sawatsky <novedevo@gmail.com>
2023-12-13 13:59:47 -0800

    day13p2

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -18,14 +18,41 @@ fn main() {
             (Some(row), None) => row * 100,
             (None, Some(col)) => col,
             (None, None) => unreachable!("neither?"),
-            (Some(_), Some(_)) => unreachable!("both?"),
+            (Some(row), Some(col)) => unreachable!("both? {row} {col}"),
         })
         .collect_vec();
 
     dbg!(&sum, sum.iter().sum::<usize>());
 }
 
-fn get_rowcols(pattern: Vec<Vec<u8>>) -> (Option<usize>, Option<usize>) {
+fn get_rowcols(mut pattern: Vec<Vec<u8>>) -> (Option<usize>, Option<usize>) {
+    let orig = get_rowcols_smudged(&pattern, (None, None));
+    for row in 0..pattern.len() {
+        for col in 0..pattern[0].len() {
+            let o = pattern[row][col];
+            pattern[row][col] = if o == b'.' { b'#' } else { b'.' };
+            let smudgy = get_rowcols_smudged(&pattern, orig);
+            if smudgy != (None, None) {
+                dbg!(
+                    orig,
+                    pattern
+                        .iter()
+                        .map(|line| String::from_utf8(line.to_vec()).unwrap())
+                        .collect_vec()
+                );
+                return smudgy;
+            }
+            let o = pattern[row][col];
+            pattern[row][col] = if o == b'.' { b'#' } else { b'.' };
+        }
+    }
+    unreachable!()
+}
+
+fn get_rowcols_smudged(
+    pattern: &[Vec<u8>],
+    (ignore_row, ignore_col): (Option<usize>, Option<usize>),
+) -> (Option<usize>, Option<usize>) {
     let mut mirror_col = None;
     for col in 1..pattern[0].len() {
         let symmetric = pattern.iter().all(|line| {
@@ -35,7 +62,7 @@ fn get_rowcols(pattern: Vec<Vec<u8>>) -> (Option<usize>, Option<usize>) {
                 .zip(line[col..].iter())
                 .all(|(a, b)| a == b)
         });
-        if symmetric {
+        if symmetric && Some(col) != ignore_col {
             mirror_col = Some(col);
             break;
         }
@@ -51,7 +78,7 @@ fn get_rowcols(pattern: Vec<Vec<u8>>) -> (Option<usize>, Option<usize>) {
                 .zip(pattern[row..].iter().map(|line| line[c]))
                 .all(|(a, b)| a == b)
         });
-        if symmetric {
+        if symmetric && Some(row) != ignore_row {
             mirror_row = Some(row);
             break;
         }
