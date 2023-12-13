779a73535317549d8cbf42674042ff54d794e422
Devon Sawatsky <novedevo@gmail.com>
2023-12-13 13:43:06 -0800

    part1done

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -4,7 +4,7 @@ use memoize::memoize;
 use rayon::iter::{ParallelBridge, ParallelIterator};
 
 fn main() {
-    let input = include_str!("../../data/day12.txt");
+    let input = include_str!("../../data/day13.txt");
     let sum = input
         .split("\n\n")
         .map(|pattern| {
@@ -14,34 +14,48 @@ fn main() {
                 .collect_vec()
         })
         .map(get_rowcols)
-        .map(|(row, col)| row * 100 + col)
+        .map(|(row, col)| match (row, col) {
+            (Some(row), None) => row * 100,
+            (None, Some(col)) => col,
+            (None, None) => unreachable!("neither?"),
+            (Some(_), Some(_)) => unreachable!("both?"),
+        })
         .collect_vec();
 
     dbg!(&sum, sum.iter().sum::<usize>());
 }
 
-fn get_rowcols(pattern: Vec<Vec<u8>>) -> (usize, usize) {
-    (1..pattern.len() - 1)
-        .map(|row| (row, 1..pattern[1].len() - 1))
-        .find_map(|(row, mut cols)| Some((row, cols.find(|col| possible(&pattern, row, *col))?)))
-        .unwrap()
-}
+fn get_rowcols(pattern: Vec<Vec<u8>>) -> (Option<usize>, Option<usize>) {
+    let mut mirror_col = None;
+    for col in 1..pattern[0].len() {
+        let symmetric = pattern.iter().all(|line| {
+            line[..col]
+                .iter()
+                .rev()
+                .zip(line[col..].iter())
+                .all(|(a, b)| a == b)
+        });
+        if symmetric {
+            mirror_col = Some(col);
+            break;
+        }
+    }
 
-fn possible(pattern: &[Vec<u8>], row: usize, col: usize) -> bool {
-    let rowable = pattern.iter().all(|line| {
-        line[..col]
-            .iter()
-            .zip(line[col..].iter().rev())
-            .all(|(a, b)| a == b)
-    });
+    let mut mirror_row = None;
+    for row in 1..pattern.len() {
+        let symmetric = (0..pattern[0].len()).all(|c| {
+            pattern[..row]
+                .iter()
+                .rev()
+                .map(|line| line[c])
+                .zip(pattern[row..].iter().map(|line| line[c]))
+                .all(|(a, b)| a == b)
+        });
+        if symmetric {
+            mirror_row = Some(row);
+            break;
+        }
+    }
 
-    let colable = (0..pattern.len()).all(|c| {
-        pattern[..row]
-            .iter()
-            .map(|line| line[c])
-            .zip(pattern[row..].iter().map(|line| line[c]))
-            .all(|(a, b)| a == b)
-    });
-
-    rowable && colable
+    (mirror_row, mirror_col)
 }
