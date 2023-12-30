6c0e20eb3aae16af8c30a1f51568b9c07315381d
Devon Burnham <novedevo@gmail.com>
2023-12-30 14:02:27 -0800

    neat idea, doesn't work

--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -33,16 +33,16 @@ fn main() {
 
     input[s_coords.0][s_coords.1] = '.';
 
-    let mut steps = vec![(s_coords.0 as isize, s_coords.1 as isize)];
+    let mut steps = vec![((s_coords.0 as isize, s_coords.1 as isize), 1usize)];
 
-    for _ in 0..500 {
+    for _ in 0..50 {
         // for (row, col) in &steps {
         //     input[*row][*col] = '.';
         // }
-        steps = steps
+        let meow = steps
             .into_iter()
-            .map(|(row, col)| (row as isize, col as isize))
-            .flat_map(|(row, col)| {
+            .map(|((row, col), count)| ((row as isize, col as isize), count))
+            .map(|((row, col), count)| {
                 let next_steps = vec![
                     (row - 1, col),
                     (row + 1, col),
@@ -50,13 +50,34 @@ fn main() {
                     (row, col + 1),
                 ];
 
-                next_steps
-                    .into_iter()
-                    // .map(|(row, col)| (row.rem_euclid(height), col.rem_euclid(width)))
-                    // .map(|(row, col)| (row as usize, col as usize))
-                    .filter(|(row, col)| input[row.rem_euclid(height) as usize][col.rem_euclid(width) as usize] != '#')
-            }).unique()
-            .collect();
+                (
+                    next_steps
+                        .into_iter()
+                        // .map(|(row, col)| (row.rem_euclid(height), col.rem_euclid(width)))
+                        // .map(|(row, col)| (row as usize, col as usize))
+                        .filter(|(row, col)| {
+                            input[row.rem_euclid(height) as usize][col.rem_euclid(width) as usize]
+                                != '#'
+                        })
+                        .collect_vec(),
+                    count,
+                )
+            });
+        let mut next_steps = HashMap::new();
+        for step in meow {
+            for sstep in step.0 {
+                let sstep = (sstep.0.rem_euclid(height), sstep.1.rem_euclid(width));
+                if let Some(ss) = next_steps.get(&sstep) {
+                    if *ss < step.1 {
+                        next_steps.insert(sstep, step.1);
+                    }
+                } else {
+                    next_steps.insert(sstep, step.1);
+                }
+            }
+        }
+
+        steps = next_steps.into_iter().collect();
 
         // for (row, col) in &steps {
         //     input[*row][*col] = 'O';
@@ -67,5 +88,5 @@ fn main() {
         // }
     }
 
-    dbg!(steps.len());
+    dbg!(steps.iter().map(|s| s.1).sum::<usize>());
 }
